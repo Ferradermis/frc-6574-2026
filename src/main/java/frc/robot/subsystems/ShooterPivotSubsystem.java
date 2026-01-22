5b1156c398f854cd86c209f35a95e4782ab1ced4
acexpp <hudanm19@whippets.wwusd.org>
2026-01-21 20:45:24 -0600

    worked on the shotter and created the pivot subsystem

---
+++ src/main/java/frc/robot/subsystems/ShooterPivotSubsystem.java
@@ -0,0 +1,43 @@
+package frc.robot.subsystems;
+
+import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.DegreesPerSecond;
+import static edu.wpi.first.units.Units.DegreesPerSecondPerSecond;
+import static edu.wpi.first.units.Units.Feet;
+import static edu.wpi.first.units.Units.Second;
+import static edu.wpi.first.units.Units.Seconds;
+import static edu.wpi.first.units.Units.Volts;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+import edu.wpi.first.math.controller.ArmFeedforward;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Voltage;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.littletonrobotics.junction.AutoLog;
+import org.littletonrobotics.junction.AutoLogOutput;
+import org.littletonrobotics.junction.Logger;
+import yams.gearing.GearBox;
+import yams.gearing.MechanismGearing;
+import yams.mechanisms.config.ArmConfig;
+import yams.mechanisms.positional.Arm;
+import yams.motorcontrollers.SmartMotorController;
+import yams.motorcontrollers.SmartMotorControllerConfig;
+import yams.motorcontrollers.SmartMotorControllerConfig.ControlMode;
+import yams.motorcontrollers.SmartMotorControllerConfig.MotorMode;
+import yams.motorcontrollers.SmartMotorControllerConfig.TelemetryVerbosity;
+import yams.motorcontrollers.remote.TalonFXWrapper;
+
+public class ShooterPivotSubsystem extends SubsystemBase {
+    
+    public class PivotConstants {
+
+        // insert variables here
+    }
+
+
+}
--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -6,12 +6,18 @@ import static edu.wpi.first.units.Units.DegreesPerSecondPerSecond;
 import static edu.wpi.first.units.Units.Inches;
 import static edu.wpi.first.units.Units.Pounds;
 import static edu.wpi.first.units.Units.RPM;
+import static edu.wpi.first.units.Units.Volts;
 
 import com.ctre.phoenix6.hardware.TalonFX;
-
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Voltage;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.littletonrobotics.junction.AutoLog;
+import org.littletonrobotics.junction.Logger;
 import yams.gearing.GearBox;
 import yams.gearing.MechanismGearing;
 import yams.mechanisms.config.FlyWheelConfig;
@@ -25,81 +31,160 @@ import yams.motorcontrollers.remote.TalonFXWrapper;
 
 public class ShooterSubsystem extends SubsystemBase {
 
-    double kP = 50;
-    double kI = 0;
-    double kD = 0;
-    double kS = 0;
-    double kV = 0;
-    double kA = 0;
+  double kP = 50;
+  double kI = 0;
+  double kD = 0;
+  double kS = 0;
+  double kV = 0;
+  double kA = 0;
 
-    private SmartMotorControllerConfig leftConfig = new SmartMotorControllerConfig(this)
-        .withControlMode(ControlMode.CLOSED_LOOP)
-        // Feedback Constants (PID Constants)
-        .withClosedLoopController(kP, kI, kD, DegreesPerSecond.of(90), DegreesPerSecondPerSecond.of(45))
-        .withSimClosedLoopController(kP, kI, kD, DegreesPerSecond.of(90), DegreesPerSecondPerSecond.of(45))
-        // Feedforward Constants
-        .withFeedforward(new SimpleMotorFeedforward(kS, kV, kA))
-        .withSimFeedforward(new SimpleMotorFeedforward(kS, kV, kA))
-        // Telemetry name and verbosity level
-        .withTelemetry("ShooterMotor", TelemetryVerbosity.HIGH)
-        // Gearing from the motor rotor to final shaft.
-        // In this example GearBox.fromReductionStages(3,4) is the same as GearBox.fromStages("3:1","4:1") which corresponds to the gearbox attached to your motor.
-        // You could also use .withGearing(12) which does the same thing.
-        .withGearing(new MechanismGearing(GearBox.fromReductionStages(3, 4)))
-        // Motor properties to prevent over currenting.
-        .withMotorInverted(false)
-        .withIdleMode(MotorMode.COAST)
-        .withStatorCurrentLimit(Amps.of(40));
+  private SmartMotorControllerConfig leftConfig =
+      new SmartMotorControllerConfig(this)
+          .withControlMode(ControlMode.CLOSED_LOOP)
+          // Feedback Constants (PID Constants)
+          .withClosedLoopController(
+              kP, kI, kD, DegreesPerSecond.of(90), DegreesPerSecondPerSecond.of(45))
+          .withSimClosedLoopController(
+              kP, kI, kD, DegreesPerSecond.of(90), DegreesPerSecondPerSecond.of(45))
+          // Feedforward Constants
+          .withFeedforward(new SimpleMotorFeedforward(kS, kV, kA))
+          .withSimFeedforward(new SimpleMotorFeedforward(kS, kV, kA))
+          // Telemetry name and verbosity level
+          .withTelemetry("ShooterMotor", TelemetryVerbosity.HIGH)
+          // Gearing from the motor rotor to final shaft.
+          // In this example GearBox.fromReductionStages(3,4) is the same as
+          // GearBox.fromStages("3:1","4:1") which corresponds to the gearbox attached to your
+          // motor.
+          // You could also use .withGearing(12) which does the same thing.
+          .withGearing(new MechanismGearing(GearBox.fromReductionStages(3, 4)))
+          // Motor properties to prevent over currenting.
+          .withMotorInverted(false)
+          .withIdleMode(MotorMode.COAST)
+          .withStatorCurrentLimit(Amps.of(40));
 
-    private SmartMotorControllerConfig rightConfig = new SmartMotorControllerConfig(this)
-        .withControlMode(ControlMode.CLOSED_LOOP)
-        // Feedback Constants (PID Constants)
-        .withClosedLoopController(kP, kI, kD, DegreesPerSecond.of(90), DegreesPerSecondPerSecond.of(45))
-        .withSimClosedLoopController(kP, kI, kD, DegreesPerSecond.of(90), DegreesPerSecondPerSecond.of(45))
-        // Feedforward Constants
-        .withFeedforward(new SimpleMotorFeedforward(kS, kV, kA))
-        .withSimFeedforward(new SimpleMotorFeedforward(kS, kV, kA))
-        // Telemetry name and verbosity level
-        .withTelemetry("ShooterMotor", TelemetryVerbosity.HIGH)
-        // Gearing from the motor rotor to final shaft.
-        // In this example GearBox.fromReductionStages(3,4) is the same as GearBox.fromStages("3:1","4:1") which corresponds to the gearbox attached to your motor.
-        // You could also use .withGearing(12) which does the same thing.
-        .withGearing(new MechanismGearing(GearBox.fromReductionStages(3, 4)))
-        // Motor properties to prevent over currenting.
-        .withMotorInverted(true)
-        .withIdleMode(MotorMode.COAST)
-        .withStatorCurrentLimit(Amps.of(40));
+  private SmartMotorControllerConfig rightConfig =
+      new SmartMotorControllerConfig(this)
+          .withControlMode(ControlMode.CLOSED_LOOP)
+          // Feedback Constants (PID Constants)
+          .withClosedLoopController(
+              kP, kI, kD, DegreesPerSecond.of(90), DegreesPerSecondPerSecond.of(45))
+          .withSimClosedLoopController(
+              kP, kI, kD, DegreesPerSecond.of(90), DegreesPerSecondPerSecond.of(45))
+          // Feedforward Constants
+          .withFeedforward(new SimpleMotorFeedforward(kS, kV, kA))
+          .withSimFeedforward(new SimpleMotorFeedforward(kS, kV, kA))
+          // Telemetry name and verbosity level
+          .withTelemetry("ShooterMotor", TelemetryVerbosity.HIGH)
+          // Gearing from the motor rotor to final shaft.
+          // In this example GearBox.fromReductionStages(3,4) is the same as
+          // GearBox.fromStages("3:1","4:1") which corresponds to the gearbox attached to your
+          // motor.
+          // You could also use .withGearing(12) which does the same thing.
+          .withGearing(new MechanismGearing(GearBox.fromReductionStages(3, 4)))
+          // Motor properties to prevent over currenting.
+          .withMotorInverted(true)
+          .withIdleMode(MotorMode.COAST)
+          .withStatorCurrentLimit(Amps.of(40));
 
-    private TalonFX leftMotor = new TalonFX(0);
-    private TalonFX rightMotor = new TalonFX(0);
+  @AutoLog
+  public static class ShooterInputs {
+    public AngularVelocity velocity = DegreesPerSecond.of(0);
+    public AngularVelocity setpoint = DegreesPerSecond.of(0);
+    public Voltage volts = Volts.of(0);
+    public Current current = Amps.of(0);
+  }
 
-    private SmartMotorController leftMotorController = new TalonFXWrapper(leftMotor, DCMotor.getKrakenX60(1), leftConfig);
-    private SmartMotorController rightMotorController = new TalonFXWrapper(rightMotor, DCMotor.getKrakenX60(1), rightConfig);
+  private final ShooterInputsAutoLogged shooterRightInputs = new ShooterInputsAutoLogged();
+  private final ShooterInputsAutoLogged shooterLeftInputs = new ShooterInputsAutoLogged();
 
-    private FlyWheelConfig leftShooterConfig = new FlyWheelConfig(leftMotorController)
-        // Diameter of the flywheel.
-        .withDiameter(Inches.of(4))
-        // Mass of the flywheel.
-        .withMass(Pounds.of(1))
-        // Maximum speed of the shooter.
-        .withUpperSoftLimit(RPM.of(1000))
-        // Telemetry name and verbosity for the shooter.
-        .withTelemetry("ShooterMech", TelemetryVerbosity.HIGH);
+  private TalonFX leftMotor = new TalonFX(0);
+  private TalonFX rightMotor = new TalonFX(0);
 
-    private FlyWheelConfig rightShooterConfig = new FlyWheelConfig(rightMotorController)
-        // Diameter of the flywheel.
-        .withDiameter(Inches.of(4))
-        // Mass of the flywheel.
-        .withMass(Pounds.of(1))
-        // Maximum speed of the shooter.
-        .withUpperSoftLimit(RPM.of(1000))
-        // Telemetry name and verbosity for the shooter.
-        .withTelemetry("ShooterMech", TelemetryVerbosity.HIGH);
+  private SmartMotorController leftMotorController =
+      new TalonFXWrapper(leftMotor, DCMotor.getKrakenX60(1), leftConfig);
+  private SmartMotorController rightMotorController =
+      new TalonFXWrapper(rightMotor, DCMotor.getKrakenX60(1), rightConfig);
 
-    private FlyWheel leftShooter = new FlyWheel(leftShooterConfig);
-    private FlyWheel rightShooter = new FlyWheel(rightShooterConfig);
+  private FlyWheelConfig leftShooterConfig =
+      new FlyWheelConfig(leftMotorController)
+          // Diameter of the flywheel.
+          .withDiameter(Inches.of(4))
+          // Mass of the flywheel.
+          .withMass(Pounds.of(1))
+          // Maximum speed of the shooter.
+          .withUpperSoftLimit(RPM.of(1000))
+          // Telemetry name and verbosity for the shooter.
+          .withTelemetry("ShooterMech", TelemetryVerbosity.HIGH);
 
-    public ShooterSubsystem() {
+  private FlyWheelConfig rightShooterConfig =
+      new FlyWheelConfig(rightMotorController)
+          // Diameter of the flywheel.
+          .withDiameter(Inches.of(4))
+          // Mass of the flywheel.
+          .withMass(Pounds.of(1))
+          // Maximum speed of the shooter.
+          .withUpperSoftLimit(RPM.of(1000))
+          // Telemetry name and verbosity for the shooter.
+          .withTelemetry("ShooterMech", TelemetryVerbosity.HIGH);
 
-    }
+  private FlyWheel leftShooter = new FlyWheel(leftShooterConfig);
+  private FlyWheel rightShooter = new FlyWheel(rightShooterConfig);
+
+  private void updateRightInputs() {
+    shooterRightInputs.velocity = rightShooter.getSpeed();
+    shooterRightInputs.setpoint =
+        rightMotorController.getMechanismSetpointVelocity().orElse(RPM.of(0));
+    shooterRightInputs.volts = rightMotorController.getVoltage();
+    shooterRightInputs.current = rightMotorController.getStatorCurrent();
+  }
+
+  public void updateLeftInputs() {
+    shooterLeftInputs.velocity = leftShooter.getSpeed();
+    shooterLeftInputs.setpoint =
+        leftMotorController.getMechanismSetpointVelocity().orElse(RPM.of(0));
+    shooterLeftInputs.volts = leftMotorController.getVoltage();
+    shooterLeftInputs.current = leftMotorController.getStatorCurrent();
+  }
+
+  public AngularVelocity getRightVelocity() {
+    return rightShooter.getSpeed();
+  }
+
+  public AngularVelocity getLeftVelocity() {
+    return leftShooter.getSpeed();
+  }
+
+  public Command setRightVelocity(AngularVelocity speed) {
+    return rightShooter.setSpeed(speed);
+  }
+
+  public Command setLeftVelocity(AngularVelocity speed) {
+    return leftShooter.setSpeed(speed);
+  }
+
+  public Command setRight(double dutyCycle) {
+    return rightShooter.set(dutyCycle);
+  }
+
+  public Command setLeft(double dutyCycle) {
+    return leftShooter.set(dutyCycle);
+  }
+
+  public ShooterSubsystem() {}
+
+  @Override
+  public void periodic() {
+    updateRightInputs();
+    Logger.processInputs("Right Shooter", shooterRightInputs);
+    rightShooter.updateTelemetry();
+    updateLeftInputs();
+    Logger.processInputs("Left Shooter", shooterLeftInputs);
+    leftShooter.updateTelemetry();
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    leftShooter.simIterate();
+    rightShooter.simIterate();
+  }
 }
